5a899743f4e9dcd0f62801f98265af29ade1dcb0
Tyrone Cloma <tyrone.cloma1@gmail.com>
2025-06-23 16:24:36 -0400

    removed .sveltekit from git

---
+++ packages/db/client.ts
@@ -0,0 +1,9 @@
+import { createClient } from "@libsql/client";
+import { drizzle } from "drizzle-orm/libsql";
+
+const client = createClient({
+    url: process.env.DB_URL!,
+    authToken: process.env.DB_AUTH_TOKEN!,
+});
+
+const db = drizzle({ client });
