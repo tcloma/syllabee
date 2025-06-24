81fcc32db0f245f13c6be4c392babe1b1064bbe9
Tyrone Cloma <tyrone.cloma1@gmail.com>
2025-06-24 11:33:15 -0400

    file upload and PDF parsing

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,5 +1,6 @@
 import { talk } from "@syllaby/core";
 import { Hono } from "hono";
+import PDF from "pdf-parse";
 
 const app = new Hono();
 
@@ -15,6 +16,28 @@ app.get("/talk", async (c) => {
 		const res = await talk(message);
 		return c.json(res);
 	}
+});
+
+app.post("/upload", async (c) => {
+	const formData = await c.req.parseBody();
+	if (!formData) {
+		return c.json({ error: "File upload is required" }, 400);
+	}
+
+	if (!formData.file || !(formData.file instanceof File)) {
+		return c.json({ error: "Invalid file upload" }, 400);
+	}
+
+	const file = formData.file as File;
+	console.log("File data received:", file);
+
+	const buffer = Buffer.from(await file.arrayBuffer());
+	console.log("File buffer size:", buffer	);
+
+	const data = await PDF(buffer)
+	console.log("PDF text extracted:", data);
+
+	return c.json({ message: "File received successfully" });
 });
 
 export default Bun.serve({
