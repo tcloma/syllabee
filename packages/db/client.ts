1648ca68d2fd038c4a294326637efe969ab82d98
Tyrone Cloma <tyrone.cloma1@gmail.com>
2025-06-26 23:56:11 -0400

    fixed client instance using invalid environment variable

--- packages/db/client.ts
+++ packages/db/client.ts
@@ -2,11 +2,10 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
 
-console.log("Process Environment:", process.env.DATABASE_URL);
-const client = postgres(process.env.DATABASE_URL as string, {
+const client = postgres(process.env.DB_URL as string, {
 	prepare: false,
 	ssl: {
-		rejectUnauthorized: false, // For local development, set to true in production
+		rejectUnauthorized: false, // Set up proper SSL configuration in production
 	},
 });
 export const db = drizzle({ client, schema });
